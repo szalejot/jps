15e5b1854067409b31a17c8b3d008d182d2d7a3a
Michal <szalejot@gmail.com>
2013-04-27 15:37:04 +0200

    W mainie jest symulacja QResSrack z cw2. Ostatni przyklad niepewny: (groupas(bag), bag) => ?

--- src/edu/pjwstk/mherman/jps/main/MainClass.java
+++ src/edu/pjwstk/mherman/jps/main/MainClass.java
@@ -1,20 +1,17 @@
 package edu.pjwstk.mherman.jps.main;
 
-import edu.pjwstk.jps.ast.IExpression;
-import edu.pjwstk.mherman.jps.ast.binary.CommaExpression;
-import edu.pjwstk.mherman.jps.ast.binary.DotExpression;
-import edu.pjwstk.mherman.jps.ast.binary.EqualsExpression;
-import edu.pjwstk.mherman.jps.ast.binary.GreaterThanExpression;
-import edu.pjwstk.mherman.jps.ast.binary.InExpression;
-import edu.pjwstk.mherman.jps.ast.binary.MinusExpression;
-import edu.pjwstk.mherman.jps.ast.binary.PlusExpression;
-import edu.pjwstk.mherman.jps.ast.binary.WhereExpression;
-import edu.pjwstk.mherman.jps.ast.terminal.DoubleTerminal;
-import edu.pjwstk.mherman.jps.ast.terminal.IntegerTerminal;
-import edu.pjwstk.mherman.jps.ast.terminal.NameTerminal;
-import edu.pjwstk.mherman.jps.ast.terminal.StringTerminal;
-import edu.pjwstk.mherman.jps.ast.unary.AvgExpression;
-import edu.pjwstk.mherman.jps.ast.unary.BagExpression;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.pjwstk.jps.result.ISingleResult;
+import edu.pjwstk.mherman.jps.datastore.SBAStore;
+import edu.pjwstk.mherman.jps.interpreter.qres.QResStack;
+import edu.pjwstk.mherman.jps.result.BagResult;
+import edu.pjwstk.mherman.jps.result.BinderResult;
+import edu.pjwstk.mherman.jps.result.BooleanResult;
+import edu.pjwstk.mherman.jps.result.IntegerResult;
+import edu.pjwstk.mherman.jps.result.StringResult;
+import edu.pjwstk.mherman.jps.result.StructResult;
 
 public class MainClass {
 
@@ -22,80 +19,172 @@ public class MainClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// mini-projekt 2
+		{
+			// (struct(1, 2+1), (bag("test", �Ala�) as nazwa));
+			QResStack qres = new QResStack();
+			// comma
+			// struct
+			// comma
+			qres.push(new IntegerResult(1));
+			qres.push(new IntegerResult(2));
+			qres.push(new IntegerResult(1));
+			IntegerResult plusRight = (IntegerResult) qres.pop();
+			IntegerResult plusLeft = (IntegerResult) qres.pop();
+			IntegerResult plusResult = new IntegerResult(plusLeft.getValue() + plusRight.getValue());
+			qres.push(plusResult);
+			ISingleResult comma1Right = (ISingleResult) qres.pop();
+			ISingleResult comma1Left = (ISingleResult) qres.pop();
+			List<ISingleResult> tmpList = new ArrayList<ISingleResult>();
+			tmpList.add(comma1Left);
+			tmpList.add(comma1Right);
+			qres.push(new BagResult(tmpList));
+			// end comma
+			BagResult tmpBag = (BagResult) qres.pop();
+			qres.push(new StructResult(new ArrayList<ISingleResult>(tmpBag.getElements())));
+			// end struct
+			//as
+			//bag
+			//comma
+			qres.push(new StringResult("test"));
+			qres.push(new StringResult("Ala"));
+			ISingleResult comma2Right = (ISingleResult) qres.pop();
+			ISingleResult comma2Left = (ISingleResult) qres.pop();
+			tmpList = new ArrayList<ISingleResult>();
+			tmpList.add(comma2Left);
+			tmpList.add(comma2Right);
+			qres.push(new BagResult(tmpList));
+			//end comma
+			tmpBag = (BagResult) qres.pop();
+			qres.push(new BagResult(new ArrayList<ISingleResult>(tmpBag.getElements())));
+			//end bag
+			BagResult tmpRes = (BagResult)qres.pop();
+			tmpList = new ArrayList<ISingleResult>();
+			for (ISingleResult isr : tmpRes.getElements()) {
+				tmpList.add(new BinderResult("nazwa", isr));
+			}
+			qres.push(new BagResult(tmpList));
+			//end as
+			BagResult comma3Right = (BagResult) qres.pop();
+			StructResult comma3Left = (StructResult) qres.pop();
+			tmpList = new ArrayList<ISingleResult>();
+			tmpList.addAll(comma3Left.elements());
+			tmpList.addAll(comma3Right.getElements());
+			qres.push(new BagResult(tmpList));
+			// end comma
+			System.out.println(qres.pop().toString());
+		}
 		
-		// Firma where (avg(zatrudnia.pensja) > 2550.50)
-		IExpression ex1 = new WhereExpression(
-							new NameTerminal("Firma"),
-							new GreaterThanExpression(
-								new AvgExpression(
-									new DotExpression(
-										new NameTerminal("zatrudnia"),
-										new NameTerminal("pensja")
-									)
-								),
-								new DoubleTerminal(2550.5)
-							)
-						);
+		{
+			//(bag("ala"+" ma"+" kota"), bag(8*10, false));
+			QResStack qres = new QResStack();
+			//comma
+			//bag
+			//left plus
+			qres.push(new StringResult("ala"));
+			//right plus
+			qres.push(new StringResult(" ma"));
+			qres.push(new StringResult(" kota"));
+			StringResult plusRight = (StringResult) qres.pop();
+			StringResult plusLeft = (StringResult) qres.pop();
+			qres.push(new StringResult(plusLeft.getValue() + plusRight.getValue()));
+			//end right plus
+			plusRight = (StringResult) qres.pop();
+			plusLeft = (StringResult) qres.pop();
+			qres.push(new StringResult(plusLeft.getValue() + plusRight.getValue()));
+			//end left plus
+			List<ISingleResult> tmpList = new ArrayList<ISingleResult>();
+			tmpList.add((ISingleResult) qres.pop());
+			qres.push(new BagResult(tmpList));
+			//end bag
+			//bag
+			//comma
+			//multiply
+			qres.push(new IntegerResult(8));
+			qres.push(new IntegerResult(10));
+			IntegerResult multRight = (IntegerResult) qres.pop();
+			IntegerResult multLeft = (IntegerResult) qres.pop();
+			qres.push(new IntegerResult(multLeft.getValue() * multRight.getValue()));
+			//end multiply
+			qres.push(new BooleanResult(false));
+			ISingleResult comma1Right = (ISingleResult) qres.pop();
+			ISingleResult comma1Left = (ISingleResult) qres.pop();
+			tmpList = new ArrayList<ISingleResult>();
+			tmpList.add(comma1Left);
+			tmpList.add(comma1Right);
+			qres.push(new BagResult(tmpList));
+			//end comma
+			BagResult tmpBag = (BagResult) qres.pop();
+			qres.push(new BagResult(new ArrayList<ISingleResult>(tmpBag.getElements())));
+			//end bag
+			BagResult comma2Right = (BagResult) qres.pop();
+			BagResult comma2Left = (BagResult) qres.pop();
+			tmpList = new ArrayList<ISingleResult>();
+			tmpList.addAll(comma2Left.getElements());
+			tmpList.addAll(comma2Right.getElements());
+			qres.push(new BagResult(tmpList));
+			//end comma
+			System.out.println(qres.pop().toString());
+		}
 		
-		// Pracownik where (adres.miasto in (bag(„Warszawa”, „Łódź”)))
-		IExpression ex2 = new WhereExpression(
-							new NameTerminal("Pracownik"),
-							new InExpression(
-								new DotExpression(
-									new NameTerminal("adres"),
-									new NameTerminal("miasto")
-								),
-								new BagExpression(
-									new CommaExpression(
-										new StringTerminal("Warszawa"),
-										new StringTerminal("Łódź")
-									)
-								)
-							)
-						);
+		{
+			//(((bag(1, 2) groupas x), bag(3, 4), 5;
+			QResStack qres = new QResStack();
+			//comma
+			//groupas
+			//bag
+			//comma
+			qres.push(new IntegerResult(1));
+			qres.push(new IntegerResult(2));
+			ISingleResult comma1Right = (ISingleResult) qres.pop();
+			ISingleResult comma1Left = (ISingleResult) qres.pop();
+			ArrayList<ISingleResult> tmpList = new ArrayList<ISingleResult>();
+			tmpList.add(comma1Left);
+			tmpList.add(comma1Right);
+			qres.push(new BagResult(tmpList));
+			//end comma
+			BagResult tmpBag = (BagResult) qres.pop();
+			qres.push(new BagResult(new ArrayList<ISingleResult>(tmpBag.getElements())));
+			//end bag
+			tmpBag = (BagResult) qres.pop();
+			qres.push(new BinderResult("x", tmpBag));
+			//end groupas
+			//right comma
+			//bag
+			//comma
+			qres.push(new IntegerResult(3));
+			qres.push(new IntegerResult(4));
+			ISingleResult comma2Right = (ISingleResult) qres.pop();
+			ISingleResult comma2Left = (ISingleResult) qres.pop();
+			tmpList = new ArrayList<ISingleResult>();
+			tmpList.add(comma2Left);
+			tmpList.add(comma2Right);
+			qres.push(new BagResult(tmpList));
+			//end comma
+			tmpBag = (BagResult) qres.pop();
+			qres.push(new BagResult(new ArrayList<ISingleResult>(tmpBag.getElements())));
+			//end bag
+			qres.push(new IntegerResult(5));
+			ISingleResult comma3Right = (ISingleResult) qres.pop();
+			BagResult comma3Left = (BagResult) qres.pop();
+			tmpList = new ArrayList<ISingleResult>();
+			tmpList.addAll(comma3Left.getElements());
+			tmpList.add(comma3Right);
+			qres.push(new BagResult(tmpList));
+			//end right comma
+			BagResult comma4Right = (BagResult) qres.pop();
+			BinderResult comma4Left = (BinderResult) qres.pop();
+			tmpList = new ArrayList<ISingleResult>();
+			tmpList.add(comma4Left);
+			tmpList.addAll(comma4Right.getElements());
+			qres.push(new BagResult(tmpList));
+			//end comma
+			System.out.println(qres.pop().toString());
+		}
 		
-		// bag(1,2+1) in bag(4-1,3-2)
-		IExpression ex3 = new InExpression(
-							new BagExpression(
-								new CommaExpression(
-									new IntegerTerminal(1),
-									new PlusExpression(
-										new IntegerTerminal(2),
-										new IntegerTerminal(1)
-									)
-								)
-							),
-							new BagExpression(
-								new CommaExpression(
-									new MinusExpression(
-										new IntegerTerminal(4),
-										new IntegerTerminal(1)
-									),
-									new MinusExpression(
-										new IntegerTerminal(3),
-										new IntegerTerminal(2)
-									)
-								)
-							)
-						);
 		
-		// (Pracownik where nazwisko=”Kowalski”).(adres where miasto=”Łódź”)
-		IExpression ex4 = new DotExpression(
-							new WhereExpression(
-								new NameTerminal("Pracownik"),
-								new EqualsExpression(
-									new NameTerminal("nazwisko"),
-									new StringTerminal("Kowalski")
-								)
-							),
-							new WhereExpression(
-								new NameTerminal("adres"),
-								new EqualsExpression(
-									new NameTerminal("miasto"),
-									new StringTerminal("Łódź")
-								)
-							)
-						);
+		SBAStore store = new SBAStore();
+		store.loadXML("..\res\test.xml");
 
 	}
 
